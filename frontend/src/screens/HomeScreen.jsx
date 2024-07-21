17ddb5058743d4fa0e9b0277a7be2a28b989f4ba
Naresh123se <naresh132na@gmail.com>
2024-07-21 23:44:51 +0545

    added discount in product

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -41,6 +41,7 @@ const HomeScreen = () => {
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
+                
               </Col>
             ))}
           </Row>
