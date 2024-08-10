41994f48794a1f93392300c864510a11bc5ba1ae
Naresh123se <naresh132na@gmail.com>
2024-08-10 21:30:58 +0545

    update

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -8,6 +8,7 @@ import Message from '../components/Message';
 import Paginate from '../components/Paginate';
 import ProductCarousel from '../components/ProductCarousel';
 import Meta from '../components/Meta';
+import '../assets/styles/index.css';
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
